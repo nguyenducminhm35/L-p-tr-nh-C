c0c433f803937bd75917f981687301bc5bd67516
nguyenducminhm35 <73330793+nguyenducminhm35@users.noreply.github.com>
2020-11-03 22:23:33 +0700

    bai 16

---
+++ bai 16/vd1 bai 16.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+  printf("\nI am in main");
+  italy();
+  brazil();
+  argentina();
+}
+italy()
+{
+	printf("\nI am in italy");
+}
+brazil()
+{
+	printf("\nI am in brazil");
+}
+argentina()
+{
+	printf("\nI am in argentina");
+}
