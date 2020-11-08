f1c1d670f1e8732434b8230fd8216ae4517055fd
nguyenducminhm35 <73330793+nguyenducminhm35@users.noreply.github.com>
2020-11-08 22:16:43 +0700

    bai17+18

---
+++ bai17+18/vd6  bai17.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+	char hotelname1[15] = "Sea View";
+	char hotelname2[15] = "Sea Breeze";
+	
+
+	
+	printf("The old name is a %s\n", hotelname1);
+	
+	strcpy(hotelname1, hotelname2);
+	/* Changes the hotel name*/
+	
+	printf("The new name is %s\n", hotelname1);
+	/* Displays the new name */
+	
+	getch();
+}
+
---
+++ bai17+18/vd7  bai17.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+	char compname[20] = "Microsoft";
+	int len, ctr;
+	
+	len = strlen(compname);
+	/* Determines the length of the string */
+	
+	for(ctr = 0; ctr < len; ctr++)
+	/* Accesses and displays each character of the string */
+	       printf("%c * ", compname[ctr]);
+	       
+	getch();
+	
+}
+
